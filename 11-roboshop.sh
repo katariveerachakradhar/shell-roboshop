cc42cd16840a963213551ff4c74409e78f643192
Veera <veeravamshi1@gmail.com>
2025-05-22 17:40:40 +0530

    Roboshop-shell

--- 11-roboshop.sh
+++ 11-roboshop.sh
@@ -28,7 +28,7 @@ do
         ,"Changes": [{
         "Action"              : "UPSERT"
         ,"ResourceRecordSet"  : {
-            "Name"              : "'$RECORD_NAME' . '$DOMAIN_NAME' "
+            "Name"              : "'$RECORD_NAME'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
